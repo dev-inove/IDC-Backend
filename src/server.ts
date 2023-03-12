bd82288bce0fca74716ada17b7277cd3a73c5979
Juan Pimentel <juandbpimentel@alu.ufc.br>
2023-03-11 22:59:35 -0300

    fix: corrigir texto de execução do server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import server from './shared/app';
 
 server.listen(process.env.SERVER_PORT, () => {
-  console.log('HTTP Server tá rodanu mo port '+process.env.SERVER_PORT+' caraio!');
+  console.log('HTTP Server está rodando no port: '+process.env.SERVER_PORT);
 })
