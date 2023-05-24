473486024c7044368138084682d3f337fe362abe
Juan Pimentel <juandbpimentel@alu.ufc.br>
2023-05-24 15:36:01 -0300

    hotfix - corrigindo configuração de módulos, sintaxe do service e adicionando pasta de interfaces

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,10 +1,11 @@
 import { Prisma } from '@prisma/client'
 import { UserRepositories } from '@repositories/UserRepositories'
+import { GeneralError, PrismaError } from 'interfaces/IErrors'
 
 const userRepositories = new UserRepositories()
 
 class UserService {
-	async delete(id: string) {
+	async Delete(id: string) {
 		try {
 			const user = await userRepositories.FindById(id)
 			if (user === null) {
@@ -13,16 +14,17 @@ class UserService {
 			return await userRepositories.Delete(id)
 		} catch (error) {
 			if (error instanceof Prisma.PrismaClientKnownRequestError) {
-				throw new Error('Erro no Prisma: ' + error.message)
+				return new PrismaError(error)
 			} else if (error instanceof Error) {
-				throw new Error('Erro geral: ' + error.message)
-			} else {
-				throw new Error('Erro indefinido | Service.delete')
-			}
+				return new GeneralError(error)
+			} else
+				return new GeneralError(
+					new Error('Erro indefinido  | ExemploService.delete'),
+				)
 		}
 	}
 
-	async update(id: string, body: { nome: string }) {
+	async Update(id: string, body: { nome: string }) {
 		try {
 			const user = await userRepositories.FindById(id)
 			if (user === null) {
@@ -31,16 +33,17 @@ class UserService {
 			return await userRepositories.Update(id, body)
 		} catch (error) {
 			if (error instanceof Prisma.PrismaClientKnownRequestError) {
-				throw new Error('Erro no Prisma: ' + error.message)
+				return new PrismaError(error)
 			} else if (error instanceof Error) {
-				throw new Error('Erro geral: ' + error.message)
-			} else {
-				throw new Error('Erro indefinido | Service.update')
-			}
+				return new GeneralError(error)
+			} else
+				return new GeneralError(
+					new Error('Erro indefinido  | ExemploService.delete'),
+				)
 		}
 	}
 
-	async create(body: {
+	async Create(body: {
 		nome: string
 		email: string
 		senha: string
@@ -50,30 +53,32 @@ class UserService {
 			return await userRepositories.Create(body)
 		} catch (error) {
 			if (error instanceof Prisma.PrismaClientKnownRequestError) {
-				throw new Error('Erro no Prisma: ' + error.message)
+				return new PrismaError(error)
 			} else if (error instanceof Error) {
-				throw new Error('Erro geral: ' + error.message)
-			} else {
-				throw new Error('Erro indefinido | Service.create')
-			}
+				return new GeneralError(error)
+			} else
+				return new GeneralError(
+					new Error('Erro indefinido  | ExemploService.delete'),
+				)
 		}
 	}
 
-	async list() {
+	async List() {
 		try {
 			return await userRepositories.List()
 		} catch (error) {
 			if (error instanceof Prisma.PrismaClientKnownRequestError) {
-				throw new Error('Erro no Prisma: ' + error.message)
+				return new PrismaError(error)
 			} else if (error instanceof Error) {
-				throw new Error('Erro geral: ' + error.message)
-			} else {
-				throw new Error('Erro indefinido | Service.list')
-			}
+				return new GeneralError(error)
+			} else
+				return new GeneralError(
+					new Error('Erro indefinido  | ExemploService.delete'),
+				)
 		}
 	}
 
-	async findByEmail(email: string) {
+	async FindByEmail(email: string) {
 		try {
 			const user = await userRepositories.FindByEmail(email)
 			if (user === null) {
@@ -82,16 +87,17 @@ class UserService {
 			return user
 		} catch (error) {
 			if (error instanceof Prisma.PrismaClientKnownRequestError) {
-				throw new Error('Erro no Prisma: ' + error.message)
+				return new PrismaError(error)
 			} else if (error instanceof Error) {
-				throw new Error('Erro geral: ' + error.message)
-			} else {
-				throw new Error('Erro indefinido | Service.findByEmail')
-			}
+				return new GeneralError(error)
+			} else
+				return new GeneralError(
+					new Error('Erro indefinido  | ExemploService.delete'),
+				)
 		}
 	}
 
-	async findById(id: string) {
+	async FindById(id: string) {
 		try {
 			const user = await userRepositories.FindById(id)
 			if (user === null) {
@@ -100,12 +106,13 @@ class UserService {
 			return user
 		} catch (error) {
 			if (error instanceof Prisma.PrismaClientKnownRequestError) {
-				throw new Error('Erro no Prisma: ' + error.message)
+				return new PrismaError(error)
 			} else if (error instanceof Error) {
-				throw new Error('Erro geral: ' + error.message)
-			} else {
-				throw new Error('Erro indefinido | Service.findByEmail')
-			}
+				return new GeneralError(error)
+			} else
+				return new GeneralError(
+					new Error('Erro indefinido  | ExemploService.delete'),
+				)
 		}
 	}
 }
