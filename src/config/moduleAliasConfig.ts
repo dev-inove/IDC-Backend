8fc9d8b9d4a1434d389d95415904cf630cf8fe2b
Juan Pimentel <juandbpimentel@alu.ufc.br>
2023-05-24 14:17:19 -0300

    hotfix - Corrigindo Módulos

--- src/config/moduleAliasConfig.ts
+++ src/config/moduleAliasConfig.ts
@@ -8,5 +8,6 @@ export default moduleAlias.addAliases({
 	'@repositories': path.join(__dirname, '..', 'repositories'),
 	'@services': path.join(__dirname, '..', 'services'),
 	'@shared': path.join(__dirname, '..', 'shared'),
+	'@tests': path.join(__dirname, '..', 'tests'),
 	'@config': path.join(__dirname),
 })
